c943763c27a574de0a4b13a1e79caf37718aabf3
K2\marko <kmarko.net@gmail.com>
2018-03-07 20:55:37 +0100

    fix

--- src/UI/src/app/appSettings.ts
+++ src/UI/src/app/appSettings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-    //public static API_ENDPOINT= "http://localhost:25054/";
-    public static API_ENDPOINT= "http://192.168.0.32:8080/";
+    public static API_ENDPOINT= "http://localhost:25054/";
+    //public static API_ENDPOINT= "http://192.168.0.32:8080/";
 }
